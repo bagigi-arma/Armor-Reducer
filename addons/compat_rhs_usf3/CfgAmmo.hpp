085f3e7ef7e0bac7e8ef77735570b6c697470e82
Fabio Schick <58027418+mrschick@users.noreply.github.com>
2025-12-14 15:46:10 +0100

    Rebalance RHSUSAF 5.56mm NATO variants

--- addons/compat_rhs_usf3/CfgAmmo.hpp
+++ addons/compat_rhs_usf3/CfgAmmo.hpp
@@ -3,4 +3,24 @@ class CfgAmmo {
 	class rhsusf_40mm_HEDP: rhsusf_40mm_HE {
 		indirectHitRange= 10.7;
 	};
+
+	class B_556x45_Ball;
+	class rhs_ammo_556x45_M193_Ball: B_556x45_Ball {
+		caliber = 0.55;//0.533881;
+	};
+	class rhs_ammo_556x45_Mk262_Ball: B_556x45_Ball {
+		caliber = 0.42;//0.677
+	};
+	class rhs_ammo_556x45_Mk318_Ball: B_556x45_Ball {
+		caliber = 0.5;//0.64
+	};
+	class rhs_ammo_556x45_M855_Ball: B_556x45_Ball {
+		caliber = 0.7;//0.44
+	};
+	class rhs_ammo_556x45_M855A1_Ball: B_556x45_Ball {
+		caliber = 0.88;//0.65
+	};
+	class rhs_ammo_556x45_M995_AP: B_556x45_Ball {
+		caliber = 0.95;//0.868621
+	};
 };
